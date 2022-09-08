d584a491e248bcc82ff6bbc1855c99e173b06c0c
G. Michael Fitzgerald <38142372+tic@users.noreply.github.com>
2022-09-08 16:21:09 -0400

    start building out action execution function

--- services/discord.service.ts
+++ services/discord.service.ts
@@ -19,6 +19,7 @@ import {
 import { config } from '../config';
 import { DiscordActionType } from '../types/serviceDiscordTypes';
 import { parseArguments } from './argumentParser.service';
+import { logMessage } from './logger.service';
 import { Semaphore } from './util.service';
 
 const options: ClientOptions = {
@@ -30,8 +31,46 @@ const options: ClientOptions = {
 };
 const client = new Client(options);
 
+const executeSemaphore = new Semaphore(1);
 const actionSemaphore = new Semaphore(1);
 const pendingActions: DiscordActionType[] = [];
+
+const executeAction = async (action: DiscordActionType) => {
+
+};
+
+const executeActions = async () => {
+  const releaseExecute = await executeSemaphore.acquire();
+  let releaseAction = await actionSemaphore.acquire();
+  try {
+    const actionsToExecute = pendingActions.splice(0);
+    for (let i = 0; i < actionsToExecute.length; i++) {
+
+    }
+  } finally {
+    releaseAction();
+    releaseExecute();
+  }
+};
+
+export const dispatchAction = async (action: DiscordActionType) => {
+  const release = await actionSemaphore.acquire();
+  try {
+    pendingActions.push(action);
+  } finally {
+    release();
+  }
+
+  executeActions();
+};
+
+export const initialize = () => {
+  client.login(config.discord.secret);
+  client.on('ready', () => {
+    logMessage('service.discord.initialize', `${client.user.tag} has logged in.`);
+    throttleActions();
+  });
+};
 
 export const buildBasicMessage = (
   content: string,
@@ -44,23 +83,6 @@ export const buildBasicMessage = (
     embeds,
   },
 );
-
-export const dispatchAction = async (action: DiscordActionType) => {
-  const release = await actionSemaphore.acquire();
-  try {
-    pendingActions.push(action);
-  } finally {
-    release();
-  }
-};
-
-export const initialize = () => {
-  client.login(config.discord.secret);
-  // client.on('ready', () => {
-  //   logMessage('service.discord.initialize', `${config.discord.username} has logged in.`);
-  //   throttleMessages();
-  // });
-};
 
 export const parseMessage = (message: Message) => {
   const isCommand = message.content.length > 1 && message.content[0] === config.discord.prefix;
